602e834822374e5f296f490b1d87d8faa56797ce
Samiul Hafiz <samiulhafiz2000@gmail.com>
2023-12-20 11:07:25 +0600

    Update 4

--- TaskManagementSystem/src/Taskmanagementsystem/database.java
+++ TaskManagementSystem/src/Taskmanagementsystem/database.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Taskmanagementsystem;
 
 import java.sql.Connection;
@@ -10,7 +5,7 @@ import java.sql.DriverManager;
 
 /**
  *
- * @author WINDOWS 10
+ * @author samiul
  */
 public class database {
     
@@ -20,7 +15,7 @@ public class database {
             
             Class.forName("com.mysql.jdbc.Driver");
             
-            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/plan", "root", ""); // root is the default username while "" or empty or null is the default password
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/taskmanagement", "root", ""); // root is the default username while "" or empty or null is the default password
             return connect;
         }catch(Exception e){e.printStackTrace();}
         return null;
--- TaskManagementSystem/src/Taskmanagementsystem/planController.java
+++ TaskManagementSystem/src/Taskmanagementsystem/planController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Taskmanagementsystem;
 
 import java.net.URL;
@@ -41,7 +36,7 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author WINDOWS 10
+ * @author samiul
  */
 public class planController implements Initializable {
 
